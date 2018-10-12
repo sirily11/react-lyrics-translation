135f9658e0636506e16c6088588b26eb6a7313f5
qiwei li <sirily1997@gmail.com>
2018-10-12 10:57:20 -0500

    Updates

---
+++ precache-manifest.8bfc634ea81b2727d9ec2ec590b3c17a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ef8ec38df02b70f206b",
+    "url": "/sirily11/react-lyrics-translation/static/js/runtime~main.0ef8ec38.js"
+  },
+  {
+    "revision": "9a5225468faea459308e",
+    "url": "/sirily11/react-lyrics-translation/static/js/main.9a522546.chunk.js"
+  },
+  {
+    "revision": "d7023385bd403278fc9e",
+    "url": "/sirily11/react-lyrics-translation/static/js/1.d7023385.chunk.js"
+  },
+  {
+    "revision": "9a5225468faea459308e",
+    "url": "/sirily11/react-lyrics-translation/static/css/main.e272b68c.chunk.css"
+  },
+  {
+    "revision": "d7023385bd403278fc9e",
+    "url": "/sirily11/react-lyrics-translation/static/css/1.85640794.chunk.css"
+  },
+  {
+    "revision": "fe544c4a2493352e3dd87ec1c3312a15",
+    "url": "/sirily11/react-lyrics-translation/index.html"
+  }
+];
